3c555ad9ca15d620816a810d16fbdb22f445cf77
stephecloutier <stephecloutier@gmail.com>
2017-06-22 03:42:16 +0200

    About page style + responsive

--- template-parts/template-about.php
+++ template-parts/template-about.php
@@ -7,17 +7,19 @@
 get_header();
 ?>
 <main class="about">
-    <h1 class="main-title">
-        <?= __('Mais qui suis-je&nbsp;?', 'sp'); ?>
-    </h1>
-    <div class="about__content">
-        <figure class="about__img--wrapper">
-            <img width="300" height="auto" src="<?= get_template_directory_uri(); ?>/assets/images/cloutierstephanie.jpg" alt="Photo de Stéphanie Cloutier">
-        </figure>
-        <p class="about__text">
-            Je m'appelle Stéphanie, j'ai 22 ans et je suis <em class="about__text--emphasis">québécoise</em> (oui, oui, du Québec, au Québec, au Canada). J'habite actuellement en Belgique, à Liège plus précisément (encore oui, à Liège dans la province de Liège). Avant de venir étudier le web au pays du chocolat, j’ai fait trois ans d’études supérieures en <em class="about__text--emphasis">design graphique</em> dans le pays des caribous (Ce qu'on appelle un <em class="about__text--emphasis">DEC</em> par chez moi).
-            J’aime beaucoup le design (web ou <em class="about__text--emphasis">print</em>), l’illustration, <em class="about__text--emphasis">Magic: The Gathering</em>, <em class="about__text--emphasis">Nintendo</em> et les pâtes au <em class="about__text--emphasis">pesto&nbsp;!</em>
-        </p>
+    <div class="about__wrapper">
+        <h1 class="main-title about__title">
+            <?= __('Mais qui suis-je&nbsp;?', 'sp'); ?>
+        </h1>
+        <div class="about__content">
+            <figure class="about__img--wrapper">
+                <img class="about__image" width="300" height="auto" src="<?= get_template_directory_uri(); ?>/assets/images/cloutierstephanie.jpg" alt="Photo de Stéphanie Cloutier">
+            </figure>
+            <p class="about__text">
+                Je m'appelle Stéphanie, j'ai 22 ans et je suis <em class="about__text--emphasis">québécoise</em> (oui, oui, du Québec, au Québec, au Canada). J'habite actuellement en Belgique, à Liège plus précisément (encore oui, à Liège dans la province de Liège). Avant de venir étudier le web au pays du chocolat, j’ai fait trois ans d’études supérieures en <em class="about__text--emphasis">design graphique</em> dans le pays des caribous (Ce qu'on appelle un <em class="about__text--emphasis">DEC</em> par chez moi).
+                J’aime beaucoup le design (web ou <em class="about__text--emphasis">print</em>), l’illustration, <em class="about__text--emphasis">Magic: The Gathering</em>, <em class="about__text--emphasis">Nintendo</em> et les pâtes au <em class="about__text--emphasis">pesto&nbsp;!</em>
+            </p>
+        </div>
     </div>
 </main>
 
