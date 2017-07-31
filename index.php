b5df131aee04431ace44ca23e0659a678cf1eefb
stephecloutier <stephecloutier@gmail.com>
2017-07-31 19:16:57 -0400

    Project hover + alignement on homepage

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ get_header();
 
 <main class="home">
 
-<!--
+
     <section class="introduction introduction--home">
         <div class="introduction__wrapper">
             <h2 class="hidden">Introduction</h2>
@@ -21,9 +21,9 @@ get_header();
             </p>
         </section>
         </div>
--->
 
-<!--
+
+
     <section class="fields">
         <div class="fields__wrapper">
             <h2 class="fields__title"><?= __('Pourquoi <span class="fields__title--emphasis">les deux&nbsp;?</em>', 'sp'); ?></h2>
@@ -33,7 +33,7 @@ get_header();
             <a href="<?= sp_get_page_url('template-about.php'); ?>" class="fields__cta cta" title="<?= __('Aller sur la page à propos', 'sp'); ?>"><?= __('Apprendre à me connaître', 'sp'); ?></a>
         </div>
     </section>
--->
+
 
     <section class="work">
         <div class="work__wrapper">
@@ -41,7 +41,7 @@ get_header();
             <p class="work__content">
                 <?= __('Voici une petite sélection de mes projets récents', 'sp'); ?>
             </p>
-            <div class="work__projects">
+            <ul class="work__projects">
                 <?php
                     $posts = $posts = new WP_Query([
                             'posts_per_page' => 3,
@@ -56,7 +56,7 @@ get_header();
                 <?php $fields = get_fields();
                         $image = $fields['project-image'];
                 ?>
-                <div class="project<?php
+                <li class="project<?php
                     if($fields['project-taxonomy']) {
                         foreach($fields['project-taxonomy'] as $taxonomy){
                             echo ' project--' . $taxonomy->slug;
@@ -64,15 +64,16 @@ get_header();
                     }
                 ?>">
                     <a href="<?= the_permalink(); ?>" class="project__link" title="<?= __('Aller sur la page du projet', 'sp'); ?>">
-                        <span class="project__title"><?= $fields['project-title']; ?></span>
                         <figure class="project__image--wrapper">
                             <img width="280" height="auto" src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" class="project__image">
+                            <div class="project__overlay"></div>
                         </figure>
+                        <span class="project__title"><?= $fields['project-title']; ?></span>
                     </a>
-                </div>
+                </li>
 
                 <?php endwhile; endif; ?>
-            </div>
+            </ul>
             <a href="<?= sp_get_page_url('template-projects.php'); ?>" class="work__cta cta" title="<?= __('Aller sur la page de mes projets', 'sp'); ?>"><?= __('Voir tous mes projets', 'sp'); ?></a>
         </div>
     </section>
