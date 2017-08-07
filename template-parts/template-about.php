d4fdc80aacf22e85e87b0e43f2678a52a1352f45
stephecloutier <stephecloutier@gmail.com>
2017-08-07 13:07:05 -0400

    Changed style on contact section

--- template-parts/template-about.php
+++ template-parts/template-about.php
@@ -16,7 +16,7 @@ get_header();
                 J'ai 22 ans, je viens de Québec et j'habite actuellement à Liège, en Belgique. J'aime beaucoup le design (web ou <em class="about__text--emphasis">print</em>), l'illustration, <em class="about__text--emphasis">Magic: The Gathering</em>, Nintendo et les pâtes au <em class="about__text--emphasis">pesto&nbsp;!</em>
             </p>
             <a href="<?= sp_get_page_url('template-projects.php'); ?>" class="about__cta cta" title="<?= __('Aller sur la page des projets', 'sp'); ?>"><?= __('Voir mes projets', 'sp'); ?></a>
-            <a href="<?= sp_get_page_url('template-contact.php'); ?>" class="about__cta cta about__cta--second" title="<?= __('Aller sur la page de contact', 'sp'); ?>"><?= __('Contactez-moi', 'sp'); ?></a>
+            <a href="#contact" class="about__cta cta about__cta--second" title="<?= __('Aller sur la section de contact', 'sp'); ?>"><?= __('Contactez-moi', 'sp'); ?></a>
         </div>
         <div class="fields">
             <section class="fields__wrapper">
@@ -28,28 +28,30 @@ get_header();
             </section>
         </div>
 
-        <div class="contact">
+        <div class="contact" id="contact">
             <section class="contact__wrapper">
                 <h2 class="contact__title"><?= __('Pour me contacter', 'sp'); ?></h2>
                 <div class="contact__infos">
-                    <span class="contact__subtitle"><?= __('Mes coordonnées', 'sp'); ?></span>
+                    <!--<span class="contact__subtitle"><?= __('Mes coordonnées', 'sp'); ?></span>-->
                     <ul class="contact__listc">
-                        <li class="contact__itemc contact__itemc--mailc">
-                            <a href="mailto:stephecloutier@gmail.com" class="contact__link" title="<?= __('M’envoyer un mail', 'sp'); ?>">stephecloutier@gmail.com</a>
-                        </li>
                         <li class="contact__itemc contact__itemc--phone">
+                            <span class="contact--special">Belgique</span>
                             +32 (0) 487 20 43 73
                         </li>
                         <li class="contact__itemc contact__itemc--address">
                             <p class="contact__itemc--line">Rue Julien Lahaut 20</p>
                             <p class="contact__itemc--line">4000 Liège</p>
-                            <p class="contact__itemc--line">Belgique</p>
+                            <!--<p class="contact__itemc--line">Belgique</p>-->
                         </li>
                         <li class="contact__itemc contact__itemc--address">
-                            <span class="contact--special">Ou encore&hellip;</span>
+                            <span class="contact--special">Canada</span>
                             <p class="contact__itemc--line">1545 avenue Du Nordet</p>
                             <p class="contact__itemc--line">Québec (Québec) G2G 1N2</p>
-                            <p class="contact__itemc--line">Canada</p>
+                            <!--<p class="contact__itemc--line">Canada</p>-->
+                        </li>
+                        <li class="contact__itemc contact__itemc--mailc">
+                            <span class="contact--special">Ou envoyez-moi simplement un mail</span>
+                            <a href="mailto:stephecloutier@gmail.com" class="contact__link" title="<?= __('M’envoyer un mail', 'sp'); ?>">stephecloutier@gmail.com</a>
                         </li>
                     </ul>
                 </div>
