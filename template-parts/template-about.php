774e87f67b8630bb7e08246eedf8188601a685c7
stephecloutier <stephecloutier@gmail.com>
2017-08-07 12:34:45 -0400

    Starting to move contact infos on about page

--- template-parts/template-about.php
+++ template-parts/template-about.php
@@ -12,29 +12,48 @@ get_header();
             <?= __('Mais qui <span class="about__title--emphasis">est Stéphanie&nbsp;?</span>', 'sp'); ?>
         </h1>
         <div class="about__content">
-            <!--
-            <figure class="about__img--wrapper">
-                <img class="about__image" width="300" height="auto" src="<?= get_template_directory_uri(); ?>/assets/images/cloutierstephanie.jpg" alt="Photo de Stéphanie Cloutier">
-            </figure>
-        -->
             <p class="about__text">
                 J'ai 22 ans, je viens de Québec et j'habite actuellement à Liège, en Belgique. J'aime beaucoup le design (web ou <em class="about__text--emphasis">print</em>), l'illustration, <em class="about__text--emphasis">Magic: The Gathering</em>, Nintendo et les pâtes au <em class="about__text--emphasis">pesto&nbsp;!</em>
-                <!--
-                Je m'appelle Stéphanie, j'ai 22 ans et je suis <em class="about__text--emphasis">québécoise</em> (oui, oui, du Québec, au Québec, au Canada). J'habite actuellement en Belgique, à Liège plus précisément (encore oui, à Liège dans la province de Liège). Avant de venir étudier le web au pays du chocolat, j’ai fait trois ans d’études supérieures en <em class="about__text--emphasis">design graphique</em> dans le pays des caribous (Ce qu'on appelle un <em class="about__text--emphasis">DEC</em> par chez moi).
-                J’aime beaucoup le design (web ou <em class="about__text--emphasis">print</em>), l’illustration, <em class="about__text--emphasis">Magic: The Gathering</em>, <em class="about__text--emphasis">Nintendo</em> et les pâtes au <em class="about__text--emphasis">pesto&nbsp;!</em>
-            -->
             </p>
             <a href="<?= sp_get_page_url('template-projects.php'); ?>" class="about__cta cta" title="<?= __('Aller sur la page des projets', 'sp'); ?>"><?= __('Voir mes projets', 'sp'); ?></a>
             <a href="<?= sp_get_page_url('template-contact.php'); ?>" class="about__cta cta about__cta--second" title="<?= __('Aller sur la page de contact', 'sp'); ?>"><?= __('Contactez-moi', 'sp'); ?></a>
         </div>
         <div class="fields">
-            <div class="fields__wrapper">
+            <section class="fields__wrapper">
                 <h2 class="fields__title"><?= __('Graphisme & <span class="fields__title--emphasis">développement web</span>', 'sp'); ?></h2>
                 <p class="fields__content">
                     <?= __('Après trois ans de design graphique, j’avais toujours envie d’étudier. Quoi de mieux que de compléter ma formation avec le développement web&nbsp;?', 'sp'); ?>
                 </p>
                 <a href="http://stephanie.cloutier.pro/cvjuillet2017.pdf" class="fields__cta cta" title="<?= __('Télécharger mon cv', 'sp'); ?>"><?= __('Télécharger mon cv', 'sp'); ?></a>
-            </div>
+            </section>
+        </div>
+
+        <div class="contact">
+            <section class="contact__wrapper">
+                <h2 class="contact__title"><?= __('Pour me contacter', 'sp'); ?></h2>
+                <div class="contact__infos">
+                    <span class="contact__subtitle"><?= __('Mes coordonnées', 'sp'); ?></span>
+                    <ul class="contact__listc">
+                        <li class="contact__itemc contact__itemc--mailc">
+                            <a href="mailto:stephecloutier@gmail.com" class="contact__link" title="<?= __('M’envoyer un mail', 'sp'); ?>">stephecloutier@gmail.com</a>
+                        </li>
+                        <li class="contact__itemc contact__itemc--phone">
+                            +32 (0) 487 20 43 73
+                        </li>
+                        <li class="contact__itemc contact__itemc--address">
+                            <p class="contact__itemc--line">Rue Julien Lahaut 20</p>
+                            <p class="contact__itemc--line">4000 Liège</p>
+                            <p class="contact__itemc--line">Belgique</p>
+                        </li>
+                        <li class="contact__itemc contact__itemc--address">
+                            <span class="contact--special">Ou encore&hellip;</span>
+                            <p class="contact__itemc--line">1545 avenue Du Nordet</p>
+                            <p class="contact__itemc--line">Québec (Québec) G2G 1N2</p>
+                            <p class="contact__itemc--line">Canada</p>
+                        </li>
+                    </ul>
+                </div>
+            </section>
         </div>
     </div>
 </main>
